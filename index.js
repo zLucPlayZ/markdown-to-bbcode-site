ef8b29704124f6e2611d81c447da3f7aad779e3d
Darren Dormer <me@darren.io>
2021-04-03 14:28:01 +0200

    run on change.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var reader = new commonmark.Parser();
-var writer = new markdown_to_bbcode.BBCodeRenderer();
-var parsed = reader.parse(document.getElementById("markdown").value);
-var result = writer.render(parsed);
-document.getElementById("bbcode").textContent = result;
+function render() {
+	var reader = new commonmark.Parser();
+	var writer = new markdown_to_bbcode.BBCodeRenderer();
+	var parsed = reader.parse(document.getElementById("markdown").value);
+	var result = writer.render(parsed);
+	document.getElementById("bbcode").textContent = result;
+}
