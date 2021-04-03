0ac7f275a03e6a28787410775d7cb9d6f448cd40
Darren Dormer <me@darren.io>
2021-04-03 14:20:35 +0200

    Add initial site.

---
+++ index.js
@@ -0,0 +1,5 @@
+var reader = new commonmark.Parser();
+var writer = new markdown_to_bbcode.BBCodeRenderer();
+var parsed = reader.parse(document.getElementById("markdown").value);
+var result = writer.render(parsed);
+document.getElementById("bbcode").textContent = result;
